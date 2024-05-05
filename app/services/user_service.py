99345eabcb8e85922dd74cd31f42055c95aff817
mishraprajwal <prajwalm882@gmail.com>
2024-05-05 18:03:29 -0400

    updated role

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -170,7 +170,11 @@ class UserService:
         if user and user.verification_token == token:
             user.email_verified = True
             user.verification_token = None  # Clear the token once used
-            user.role = UserRole.AUTHENTICATED
+
+            # Check if the current role is ANONYMOUS and then update to AUTHENTICATED
+            if user.role == UserRole.ANONYMOUS:
+                user.role = UserRole.AUTHENTICATED
+
             session.add(user)
             await session.commit()
             return True
