f1ccd28117f760cd6db7fd4a82352fc26ad1c379
Ramya-Ravisankar <rb756@njit.edu>
2024-05-06 02:38:52 -0400

    issue 1 email verification fix - user_service.py file changes

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -65,16 +65,13 @@ class UserService:
             new_user.nickname = new_nickname
             logger.info(f"User Role: {new_user.role}")
             user_count = await cls.count(session)
-            new_user.role = UserRole.ADMIN if user_count == 0 else UserRole.ANONYMOUS            
+            new_user.role = UserRole.ADMIN if user_count == 0 else UserRole.ANONYMOUS
             if new_user.role == UserRole.ADMIN:
                 new_user.email_verified = True
-
-            else:
                 new_user.verification_token = generate_verification_token()
+                session.add(new_user)
+                await session.commit()
                 await email_service.send_verification_email(new_user)
-
-            session.add(new_user)
-            await session.commit()
             return new_user
         except ValidationError as e:
             logger.error(f"Validation error during user creation: {e}")
@@ -121,7 +118,7 @@ class UserService:
     @classmethod
     async def register_user(cls, session: AsyncSession, user_data: Dict[str, str], get_email_service) -> Optional[User]:
         return await cls.create(session, user_data, get_email_service)
-    
+
 
     @classmethod
     async def login_user(cls, session: AsyncSession, email: str, password: str) -> Optional[User]:
@@ -170,7 +167,8 @@ class UserService:
         if user and user.verification_token == token:
             user.email_verified = True
             user.verification_token = None  # Clear the token once used
-            user.role = UserRole.AUTHENTICATED
+            if user.role == UserRole.ANONYMOUS:
+                user.role = UserRole.AUTHENTICATED
             session.add(user)
             await session.commit()
             return True
@@ -188,7 +186,7 @@ class UserService:
         result = await session.execute(query)
         count = result.scalar()
         return count
-    
+
     @classmethod
     async def unlock_user_account(cls, session: AsyncSession, user_id: UUID) -> bool:
         user = await cls.get_by_id(session, user_id)
