701a0b2a0a89c437eead4af9c0541c0f5f506376
gbj3 <gbj3@njit.edu>
2024-05-05 22:54:20 -0400

    fixed prior tests

--- tests/test_schemas/test_user_schemas.py
+++ tests/test_schemas/test_user_schemas.py
@@ -111,18 +111,18 @@ def test_user_base_url_invalid(url, user_base_data):
 
 # Test username with length less than 3 characters
 def test_user_base_username_length_too_short(user_base_data):
-    user_base_data["username"] = "ab"
+    user_base_data["nickname"] = "ab"
     with pytest.raises(ValidationError):
         UserBase(**user_base_data)
 
     # Test username with length equal to 3 characters
-    user_base_data["username"] = "abc"
+    user_base_data["nickname"] = "abc"
     user = UserBase(**user_base_data)
     assert user.username == "abc"
 
 # Test username with invalid special characters
 def test_user_base_username_invalid_char(user_base_data):
-    user_base_data["username"] = "@()/\!"
+    user_base_data["nickname"] = "@()/\!"
     with pytest.raises(ValidationError):
         UserBase(**user_base_data)
 
