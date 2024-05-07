6dd794257647a7e121eab7865ead5aa1f809f144
vaniallla <vgao825@gmail.com>
2024-05-07 10:17:38 -0400

    added API endpoint to update profile fields

--- app/routers/user_routes.py
+++ app/routers/user_routes.py
@@ -1,21 +1,29 @@
 """
-This Python file is part of a FastAPI application, demonstrating user management functionalities including creating, reading,
-updating, and deleting (CRUD) user information. It uses OAuth2 with Password Flow for security, ensuring that only authenticated
-users can perform certain operations. Additionally, the file showcases the integration of FastAPI with SQLAlchemy for asynchronous
-database operations, enhancing performance by non-blocking database calls.
+This Python file is part of a FastAPI application, demonstrating user 
+management functionalities including creating, reading, updating, and deleting (CRUD) user 
+information. It uses OAuth2 with Password Flow for security, ensuring that only authenticated 
+users can perform certain operations. Additionally, the file showcases the integration of 
+FastAPI with SQLAlchemy for asynchronous database operations, enhancing performance 
+by non-blocking database calls.
 
-The implementation emphasizes RESTful API principles, with endpoints for each CRUD operation and the use of HTTP status codes
-and exceptions to communicate the outcome of operations. It introduces the concept of HATEOAS (Hypermedia as the Engine of
-Application State) by including navigational links in API responses, allowing clients to discover other related operations dynamically.
+The implementation emphasizes RESTful API principles, with endpoints for each 
+CRUD operation and the use of HTTP status codes and exceptions to communicate the 
+outcome of operations. It introduces the concept of HATEOAS (Hypermedia as the Engine of
+Application State) by including navigational links in API responses, 
+allowing clients to discover other related operations dynamically.
 
-OAuth2PasswordBearer is employed to extract the token from the Authorization header and verify the user's identity, providing a layer
-of security to the operations that manipulate user data.
+OAuth2PasswordBearer is employed to extract the token from the Authorization header 
+and verify the user's identity, providing a layer of security to the operations 
+that manipulate user data.
 
 Key Highlights:
 - Use of FastAPI's Dependency Injection system to manage database sessions and user authentication.
-- Demonstrates how to perform CRUD operations in an asynchronous manner using SQLAlchemy with FastAPI.
-- Implements HATEOAS by generating dynamic links for user-related actions, enhancing API discoverability.
-- Utilizes OAuth2PasswordBearer for securing API endpoints, requiring valid access tokens for operations.
+- Demonstrates how to perform CRUD operations in an asynchronous manner using SQLAlchemy with 
+    FastAPI.
+- Implements HATEOAS by generating dynamic links for user-related actions, enhancing API 
+    discoverability.
+- Utilizes OAuth2PasswordBearer for securing API endpoints, requiring valid access tokens 
+    for operations.
 """
 
 from builtins import dict, int, len, str
@@ -24,10 +32,9 @@ from uuid import UUID
 from fastapi import APIRouter, Depends, HTTPException, Response, status, Request
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.dependencies import get_current_user, get_db, get_email_service, require_role
-from app.schemas.pagination_schema import EnhancedPagination
+from app.dependencies import get_db, get_email_service, require_role
 from app.schemas.token_schema import TokenResponse
-from app.schemas.user_schemas import LoginRequest, UserBase, UserCreate, UserListResponse, UserResponse, UserUpdate
+from app.schemas.user_schemas import UserCreate, UserListResponse, UserResponse, UserUpdate
 from app.services.user_service import UserService
 from app.services.jwt_service import create_access_token
 from app.utils.link_generation import create_user_links, generate_pagination_links
@@ -36,13 +43,17 @@ from app.services.email_service import EmailService
 router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 settings = get_settings()
-@router.get("/users/{user_id}", response_model=UserResponse, name="get_user", tags=["User Management Requires (Admin or Manager Roles)"])
-async def get_user(user_id: UUID, request: Request, db: AsyncSession = Depends(get_db), token: str = Depends(oauth2_scheme), current_user: dict = Depends(require_role(["ADMIN", "MANAGER"]))):
+@router.get("/users/{user_id}", response_model=UserResponse, name="get_user", 
+            tags=["User Management Requires (Admin or Manager Roles)"])
+async def get_user(user_id: UUID, request: Request, db: AsyncSession = Depends(get_db), 
+                   token: str = Depends(oauth2_scheme), 
+                   current_user: dict = Depends(require_role(["ADMIN", "MANAGER"]))):
     """
     Endpoint to fetch a user by their unique identifier (UUID).
 
-    Utilizes the UserService to query the database asynchronously for the user and constructs a response
-    model that includes the user's details along with HATEOAS links for possible next actions.
+    Utilizes the UserService to query the database asynchronously for 
+    the user and constructs a response model that includes the user's details 
+    along with HATEOAS links for possible next actions.
 
     Args:
         user_id: UUID of the user to fetch.
@@ -72,14 +83,19 @@ async def get_user(user_id: UUID, request: Request, db: AsyncSession = Depends(g
     )
 
 # Additional endpoints for update, delete, create, and list users follow a similar pattern, using
-# asynchronous database operations, handling security with OAuth2PasswordBearer, and enhancing response
-# models with dynamic HATEOAS links.
+# asynchronous database operations, handling security with OAuth2PasswordBearer,
+# and enhancing response models with dynamic HATEOAS links.
 
-# This approach not only ensures that the API is secure and efficient but also promotes a better client
-# experience by adhering to REST principles and providing self-discoverable operations.
+# This approach not only ensures that the API is secure and efficient but 
+# also promotes a better client experience by adhering to REST 
+# principles and providing self-discoverable operations.
 
-@router.put("/users/{user_id}", response_model=UserResponse, name="update_user", tags=["User Management Requires (Admin or Manager Roles)"])
-async def update_user(user_id: UUID, user_update: UserUpdate, request: Request, db: AsyncSession = Depends(get_db), token: str = Depends(oauth2_scheme), current_user: dict = Depends(require_role(["ADMIN", "MANAGER"]))):
+@router.put("/users/{user_id}", response_model=UserResponse, name="update_user", 
+            tags=["User Management Requires (Admin or Manager Roles)"])
+async def update_user(user_id: UUID, user_update: UserUpdate, 
+                      request: Request, db: AsyncSession = Depends(get_db), 
+                      token: str = Depends(oauth2_scheme), 
+                      current_user: dict = Depends(require_role(["ADMIN", "MANAGER"]))):
     """
     Update user information.
 
@@ -109,8 +125,11 @@ async def update_user(user_id: UUID, user_update: UserUpdate, request: Request, 
     )
 
 
-@router.delete("/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT, name="delete_user", tags=["User Management Requires (Admin or Manager Roles)"])
-async def delete_user(user_id: UUID, db: AsyncSession = Depends(get_db), token: str = Depends(oauth2_scheme), current_user: dict = Depends(require_role(["ADMIN", "MANAGER"]))):
+@router.delete("/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT, name="delete_user", 
+               tags=["User Management Requires (Admin or Manager Roles)"])
+async def delete_user(user_id: UUID, db: AsyncSession = Depends(get_db), 
+                      token: str = Depends(oauth2_scheme), 
+                      current_user: dict = Depends(require_role(["ADMIN", "MANAGER"]))):
     """
     Delete a user by their ID.
 
@@ -123,8 +142,12 @@ async def delete_user(user_id: UUID, db: AsyncSession = Depends(get_db), token: 
 
 
 
-@router.post("/users/", response_model=UserResponse, status_code=status.HTTP_201_CREATED, tags=["User Management Requires (Admin or Manager Roles)"], name="create_user")
-async def create_user(user: UserCreate, request: Request, db: AsyncSession = Depends(get_db), email_service: EmailService = Depends(get_email_service), token: str = Depends(oauth2_scheme), current_user: dict = Depends(require_role(["ADMIN", "MANAGER"]))):
+@router.post("/users/", response_model=UserResponse, status_code=status.HTTP_201_CREATED, 
+             tags=["User Management Requires (Admin or Manager Roles)"], name="create_user")
+async def create_user(user: UserCreate, request: Request, db: AsyncSession = Depends(get_db), 
+                      email_service: EmailService = Depends(get_email_service), 
+                      token: str = Depends(oauth2_scheme), 
+                      current_user: dict = Depends(require_role(["ADMIN", "MANAGER"]))):
     """
     Create a new user.
 
@@ -143,12 +166,12 @@ async def create_user(user: UserCreate, request: Request, db: AsyncSession = Dep
     existing_user = await UserService.get_by_email(db, user.email)
     if existing_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already exists")
-    
+
     created_user = await UserService.create(db, user.model_dump(), email_service)
     if not created_user:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to create user")
-    
-    
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+                            detail="Failed to create user")
+
     return UserResponse.model_construct(
         id=created_user.id,
         bio=created_user.bio,
@@ -165,7 +188,8 @@ async def create_user(user: UserCreate, request: Request, db: AsyncSession = Dep
     )
 
 
-@router.get("/users/", response_model=UserListResponse, tags=["User Management Requires (Admin or Manager Roles)"])
+@router.get("/users/", response_model=UserListResponse, 
+            tags=["User Management Requires (Admin or Manager Roles)"])
 async def list_users(
     request: Request,
     skip: int = 0,
@@ -179,9 +203,9 @@ async def list_users(
     user_responses = [
         UserResponse.model_validate(user) for user in users
     ]
-    
+
     pagination_links = generate_pagination_links(request, skip, limit, total_users)
-    
+
     # Construct the final response with pagination details
     return UserListResponse(
         items=user_responses,
@@ -200,9 +224,11 @@ async def register(user_data: UserCreate, session: AsyncSession = Depends(get_db
     raise HTTPException(status_code=400, detail="Email already exists")
 
 @router.post("/login/", response_model=TokenResponse, tags=["Login and Registration"])
-async def login(form_data: OAuth2PasswordRequestForm = Depends(), session: AsyncSession = Depends(get_db)):
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), 
+                session: AsyncSession = Depends(get_db)):
     if await UserService.is_account_locked(session, form_data.username):
-        raise HTTPException(status_code=400, detail="Account locked due to too many failed login attempts.")
+        raise HTTPException(status_code=400, 
+                            detail="Account locked due to too many failed login attempts.")
 
     user = await UserService.login_user(session, form_data.username, form_data.password)
     if user:
@@ -216,26 +242,31 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends(), session: Async
         return {"access_token": access_token, "token_type": "bearer"}
     raise HTTPException(status_code=401, detail="Incorrect email or password.")
 
-@router.post("/login/", include_in_schema=False, response_model=TokenResponse, tags=["Login and Registration"])
-async def login(form_data: OAuth2PasswordRequestForm = Depends(), session: AsyncSession = Depends(get_db)):
-    if await UserService.is_account_locked(session, form_data.username):
-        raise HTTPException(status_code=400, detail="Account locked due to too many failed login attempts.")
+# @router.post("/login/", include_in_schema=False, response_model=TokenResponse, 
+#              tags=["Login and Registration"])
+# async def login(form_data: OAuth2PasswordRequestForm = Depends(), 
+#                 session: AsyncSession = Depends(get_db)):
+#     if await UserService.is_account_locked(session, form_data.username):
+#         raise HTTPException(status_code=400, 
+#                             detail="Account locked due to too many failed login attempts.")
 
-    user = await UserService.login_user(session, form_data.username, form_data.password)
-    if user:
-        access_token_expires = timedelta(minutes=settings.access_token_expire_minutes)
+#     user = await UserService.login_user(session, form_data.username, form_data.password)
+#     if user:
+#         access_token_expires = timedelta(minutes=settings.access_token_expire_minutes)
 
-        access_token = create_access_token(
-            data={"sub": user.email, "role": str(user.role.name)},
-            expires_delta=access_token_expires
-        )
+#         access_token = create_access_token(
+#             data={"sub": user.email, "role": str(user.role.name)},
+#             expires_delta=access_token_expires
+#         )
 
-        return {"access_token": access_token, "token_type": "bearer"}
-    raise HTTPException(status_code=401, detail="Incorrect email or password.")
+#         return {"access_token": access_token, "token_type": "bearer"}
+#     raise HTTPException(status_code=401, detail="Incorrect email or password.")
 
 
-@router.get("/verify-email/{user_id}/{token}", status_code=status.HTTP_200_OK, name="verify_email", tags=["Login and Registration"])
-async def verify_email(user_id: UUID, token: str, db: AsyncSession = Depends(get_db), email_service: EmailService = Depends(get_email_service)):
+@router.get("/verify-email/{user_id}/{token}", status_code=status.HTTP_200_OK, 
+            name="verify_email", tags=["Login and Registration"])
+async def verify_email(user_id: UUID, token: str, db: AsyncSession = Depends(get_db), 
+                       email_service: EmailService = Depends(get_email_service)):
     """
     Verify user's email with a provided token.
     
@@ -244,4 +275,48 @@ async def verify_email(user_id: UUID, token: str, db: AsyncSession = Depends(get
     """
     if await UserService.verify_email_with_token(db, user_id, token):
         return {"message": "Email verified successfully"}
-    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid or expired verification token")
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
+                        detail="Invalid or expired verification token")
+
+@router.put("/users/{user_id}/profile", response_model=UserResponse, name="update_user_profile", 
+            tags=["User Management Requires (Admin or Manager Roles)"])
+async def update_user_profile(
+    user_id: UUID,
+    user_update: UserUpdate,
+    request: Request,
+    db: AsyncSession = Depends(get_db),
+    token: str = Depends(oauth2_scheme),
+    current_user: dict = Depends(require_role(["ADMIN", "MANAGER"]))
+):
+    """
+    Update user profile information.
+
+    Args:
+        - user_id: UUID of the user whose profile is to be updated.
+        - user_update: UserUpdate model with updated profile information.
+        - request: The request object, used to generate full URLs in the response.
+        - db: Dependency that provides an AsyncSession for database access.
+        - token: The OAuth2 access token obtained through OAuth2PasswordBearer dependency.
+        - current_user: Dependency that provides information about the current authenticated user.
+    """
+    user_data = user_update.model_dump(exclude_unset=True)
+    updated_user = await UserService.update(db, user_id, user_data)
+    if not updated_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+    return UserResponse.model_construct(
+        id=updated_user.id,
+        bio=updated_user.bio,
+        first_name=updated_user.first_name,
+        last_name=updated_user.last_name,
+        nickname=updated_user.nickname,
+        email=updated_user.email,
+        role=updated_user.role,
+        last_login_at=updated_user.last_login_at,
+        profile_picture_url=updated_user.profile_picture_url,
+        github_profile_url=updated_user.github_profile_url,
+        linkedin_profile_url=updated_user.linkedin_profile_url,
+        created_at=updated_user.created_at,
+        updated_at=updated_user.updated_at,
+        links=create_user_links(updated_user.id, request)
+    )
