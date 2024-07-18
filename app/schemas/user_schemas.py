64cbb67defe7130d82e95566aa9f36057a7cecea
ajb426 <ajb42686@yahoo.com>
2024-07-17 22:02:31 -0400

    updated role field to be UserRole type in UserUpdate

--- app/schemas/user_schemas.py
+++ app/schemas/user_schemas.py
@@ -46,7 +46,7 @@ class UserUpdate(UserBase):
     profile_picture_url: Optional[str] = Field(None, example="https://example.com/profiles/john.jpg")
     linkedin_profile_url: Optional[str] =Field(None, example="https://linkedin.com/in/johndoe")
     github_profile_url: Optional[str] = Field(None, example="https://github.com/johndoe")
-    role: Optional[str] = Field(None, example="AUTHENTICATED")
+    role: Optional[UserRole] = Field(None, example=UserRole.AUTHENTICATED)
 
     @root_validator(pre=True)
     def check_at_least_one_value(cls, values):
